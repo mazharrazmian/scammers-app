c782ffd4dd96b0a909a4d200270263dc59ca58f2
unknown <mazhar.ali1996@yahoo.com>
2020-10-03 22:39:49 +0500

    Updates

---
+++ precache-manifest.efe8483c7c43258ea12d1a34083afbb8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deb2f8769cc52df8729805ed70e137ce",
+    "url": "/scammers-app/index.html"
+  },
+  {
+    "revision": "7bda46eed08bd64e5ed3",
+    "url": "/scammers-app/static/css/2.5a510433.chunk.css"
+  },
+  {
+    "revision": "f5efb5df6c9cebbb83c3",
+    "url": "/scammers-app/static/css/main.50f1a70a.chunk.css"
+  },
+  {
+    "revision": "7bda46eed08bd64e5ed3",
+    "url": "/scammers-app/static/js/2.a8a5ffb2.chunk.js"
+  },
+  {
+    "revision": "4f98be436b88b05bb27c5253e1cce92a",
+    "url": "/scammers-app/static/js/2.a8a5ffb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5efb5df6c9cebbb83c3",
+    "url": "/scammers-app/static/js/main.47cd2346.chunk.js"
+  },
+  {
+    "revision": "3f3c9a0a29368c960738",
+    "url": "/scammers-app/static/js/runtime-main.fa1c0db3.js"
+  },
+  {
+    "revision": "27c9711a2caeea793ee0bb4c91c53b5e",
+    "url": "/scammers-app/static/media/scammer1.27c9711a.png"
+  }
+]);
